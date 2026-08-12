26fd7316a193ab31e69f125a43f71211a12b6ea0
ThanhTran(KaitoKid) <thanhtran2005isme@gmail.com>
2026-08-13 03:15:29 +0700

    fix(frontend): render toast notifications for auth errors

--- kaito-kid-react/src/main.tsx
+++ kaito-kid-react/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 
 // CSS gốc từ project cũ
@@ -82,5 +83,6 @@ registerServiceWorker();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
   </StrictMode>,
 )
