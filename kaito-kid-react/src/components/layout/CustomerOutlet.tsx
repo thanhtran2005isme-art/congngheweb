739d6793e16bbe69e61d2546983171dc3a47efe5
ThanhTran(KaitoKid) <thanhtran2005isme@gmail.com>
2026-08-13 23:54:42 +0700

    refactor(account): replace outlet with guarded notification resolver

--- kaito-kid-react/src/components/layout/CustomerOutlet.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, useLocation } from 'react-router-dom';
-import OffersView from '../../pages/OffersView';
-
-export default function CustomerOutlet() {
-  const location = useLocation();
-  const showOffers = location.pathname === '/account'
-    && new URLSearchParams(location.search).get('tab') === 'notifications';
-  return showOffers ? <OffersView /> : <Outlet />;
-}
